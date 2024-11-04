24e10429ab6705de4c8243a589cbda6f6ba9b3db
Patrick Cuppi <cuppi.patrick@gmail.com>
2024-11-04 16:22:38 -0300

    feat: added routes and User types

---
+++ src/routes.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import UserCreate from "./pages/Users/Create";
+import UserEdit from "./pages/Users/Edit";
+import UserList from "./pages/Users/List";
+
+export default function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/users">
+                <Route path="/users" element={<UserList />}></Route>
+                <Route path="/users/new" element={<UserCreate />}></Route>
+                <Route path="/users/:id" element={<UserEdit />}></Route>
+            </Route>
+        </Routes>
+    )
+}
