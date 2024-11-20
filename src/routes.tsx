c04d65bb75acf3804b4a3e608421ce6157c2b167
Patrick Cuppi <cuppi.patrick@gmail.com>
2024-11-20 16:11:52 -0300

    feat: user schema finished

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import UserCreate from "./pages/Users/Create";
 import UserEdit from "./pages/Users/Edit";
 import UserList from "./pages/Users/List";
+import { Dashboard } from "@mui/icons-material";
 
 export default function AppRoutes() {
     return (
         <Routes>
+            <Route path="/" element={<Dashboard />} />
             <Route path="/users">
                 <Route path="/users" element={<UserList />}></Route>
                 <Route path="/users/new" element={<UserCreate />}></Route>
